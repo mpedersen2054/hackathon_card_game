3742b04c487b4445ab1bc15826d2e78af0453e1f
Matt Pedersen <mattpp@Matts-MacBook-Air.local>
2017-02-09 11:07:53 -0800

    asd

--- Program.cs
+++ Program.cs
@@ -11,6 +11,7 @@ namespace ConsoleApplication
 
             Player matt = new Player("Matt");
             Player dealer = new Player("Dealer");
+            
 
             matt.Draw(deck);
             matt.Draw(deck);
